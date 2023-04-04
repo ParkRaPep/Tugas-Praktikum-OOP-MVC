f2153a4fb42eadb920eb42d6d9754212f1eb7ecb
ParkRaPep <129540875+ParkRaPep@users.noreply.github.com>
2023-04-04 22:00:26 +0700

    Add files via upload

---
+++ Lat4_1_after.php
@@ -0,0 +1,26 @@
+<?php
+    //class mobil
+    class mobil {
+        public $name;
+        public $merk;
+        public $a;
+
+        function getWarna() {
+            return $this->warna = $a;
+        }
+
+        function getInfo() {
+            echo "Nama mobil : ".$this->nama."<br>";
+            echo "Merk : ".$this->merk."<br>";
+            echo "Warna : ".$this->warna."<br>";
+        }
+    }
+
+    //bagian main
+    $ferari = new mobil();
+    $ferari->nama = "xxx";
+    $ferari->merk = "aaa";
+    $ferari->warna = "merah";
+
+    $ferari->getInfo();
+?>
